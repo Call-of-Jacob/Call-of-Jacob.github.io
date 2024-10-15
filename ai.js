58fbfa6231411bd205e2467bc4cc412754cc37df
Call-of-Jacob <kaigarvin74@gmail.com>
2024-10-15 16:15:18 -0700

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,15 @@
+export class AIController {
+  constructor(scene) {
+    this.enemies = [];
+  }
+
+  spawnEnemy(position) {
+    // Load enemy model and add to scene
+  }
+
+  updateAI() {
+    this.enemies.forEach(enemy => {
+      // AI decision making
+    });
+  }
+}
---
+++ physics.js
@@ -0,0 +1,11 @@
+import * as Ammo from 'ammo.js';
+
+export class AmmoPhysics {
+  constructor(scene) {
+    // Initialize Ammo.js physics engine
+  }
+
+  update() {
+    // Update physics simulation
+  }
+}
